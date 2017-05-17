752e51bb9241bf2ebe87f52e9247390f4184ac6f
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-05-17 13:53:20 +0300

    added functionality to change background color on fact change

--- app/src/main/java/com/jimymuriuki/chuckfacts/ChuckFactActivity.java
+++ app/src/main/java/com/jimymuriuki/chuckfacts/ChuckFactActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -14,12 +15,14 @@ import butterknife.ButterKnife;
 public class ChuckFactActivity extends AppCompatActivity implements View.OnClickListener {
     private Resources resources;
     private String[] chuckFacts;
+    private int[] backgroundColors;
 
 
     //define the views
     @Bind(R.id.tv_chuck_fact_title)TextView chuckFactTitle;
     @Bind(R.id.tv_chuck_fact)TextView chuckFact;
     @Bind(R.id.btn_changeFact)Button changeFact;
+    @Bind(R.id.linearLayout)LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +31,21 @@ public class ChuckFactActivity extends AppCompatActivity implements View.OnClick
         ButterKnife.bind(this);
 
         resources=getResources();
+        //getting the array from the resources
         chuckFacts=resources.getStringArray(R.array.chuck_jokes_array);
+        backgroundColors=resources.getIntArray(R.array.background_colors);
+
+        //getting random fact
         String randomFact=RandomGenerator.getRandomFact(chuckFacts);
+        //int color=
+        int backgroundColor=RandomGenerator.getRandomColor(backgroundColors);
+
+        linearLayout.setBackgroundColor(backgroundColor);
+        changeFact.setBackgroundColor(backgroundColor);
         chuckFact.setText(randomFact);
+
+
+
         changeFact.setOnClickListener(this);
 
 
@@ -41,6 +56,10 @@ public class ChuckFactActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View v) {
         String randomFact=RandomGenerator.getRandomFact(chuckFacts);
+        int backgroundColor=RandomGenerator.getRandomColor(backgroundColors);
+        linearLayout.setBackgroundColor(backgroundColor);
+        changeFact.setBackgroundColor(backgroundColor);
         chuckFact.setText(randomFact);
+
     }
 }
