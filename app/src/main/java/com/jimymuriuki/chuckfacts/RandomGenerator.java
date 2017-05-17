ea2e81cb6c1657c36d236e81a0e678a6571cbc94
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-05-17 13:45:10 +0300

    added a get random color method in the random generator class

--- app/src/main/java/com/jimymuriuki/chuckfacts/RandomGenerator.java
+++ app/src/main/java/com/jimymuriuki/chuckfacts/RandomGenerator.java
@@ -1,14 +1,22 @@
 package com.jimymuriuki.chuckfacts;
 
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomGenerator {
 
     public static String getRandomFact(String[] chuckFacts){
-        Random random=new Random();
 
+        Random random=new Random();
         String randomFact=chuckFacts[random.nextInt(chuckFacts.length)];
+        return randomFact;
+
+    }
+    public static int getRandomColor(int[] colors){
+
+        Random random=new Random();
+        int randomFact=colors[random.nextInt(colors.length)];
         return randomFact;
 
     }
