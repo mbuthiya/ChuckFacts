170f85b4e3717fc351ab6582019f2ba4b4a8bfb6
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-05-17 11:21:23 +0300

    Managed to target one fact
    1. Got the arrary from the resourse directory and stored them in a string array

--- app/src/main/java/com/jimymuriuki/chuckfacts/ChuckFactActivity.java
+++ app/src/main/java/com/jimymuriuki/chuckfacts/ChuckFactActivity.java
@@ -1,13 +1,42 @@
 package com.jimymuriuki.chuckfacts;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ChuckFactActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ChuckFactActivity extends AppCompatActivity implements View.OnClickListener {
+    private Resources resources;
+    private String[] chuckFacts;
+
+    //define the views
+    @Bind(R.id.tv_chuck_fact_title)TextView chuckFactTitle;
+    @Bind(R.id.tv_chuck_fact)TextView chuckFact;
+    @Bind(R.id.btn_changeFact)Button changeFact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chuck_fact);
+        ButterKnife.bind(this);
+
+        resources=getResources();
+        chuckFacts=resources.getStringArray(R.array.chuck_jokes_array);
+        chuckFact.setText(chuckFacts[0]);
+
+        changeFact.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
